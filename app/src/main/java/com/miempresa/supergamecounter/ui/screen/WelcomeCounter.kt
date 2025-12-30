3d69abcce43cb35caadc2341756178cf64a91117
anibaL <a88anibal@gmail.com>
2025-12-30 07:41:54 +0100

    Cambios simples

--- app/src/main/java/com/miempresa/supergamecounter/ui/screen/WelcomeCounter.kt
+++ app/src/main/java/com/miempresa/supergamecounter/ui/screen/WelcomeCounter.kt
@@ -1,18 +1,23 @@
 package com.miempresa.supergamecounter.ui.screen
 
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun RootWelcomeCounter(name: String?) {
-    var name by remember { mutableStateOf("") }
+
     var score by remember { mutableIntStateOf(0) }
 
     //Calculado automáticamente
@@ -21,15 +26,54 @@ fun RootWelcomeCounter(name: String?) {
         mutableIntStateOf((score / 5).coerceAtMost(5))
     }
 
-    //Funciones
-    fun increaseScore() {
-        score++
+
+    WelcomeScreenCounter(
+        name = name,
+        score = score,
+        level = level,
+        onIncrease = { score++ }
+
+
+    )
+
+}
+
+
+fun getLevelColor(level: Int): Color {
+    return when (level % 10) {
+        0 -> Color.Magenta
+        5 -> Color.Green
+        else -> Color.Cyan
     }
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun WelcomeScreenCounter() {
+fun WelcomeScreenCounter(name: String?, score: Int, level: Int, onIncrease: () -> Unit) {
+
+    val levelColor = getLevelColor(level)
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(
+                    "Super Counter Game",
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        color = Color.Red
+                    )
+                )
+            })
+        }
+    ) {
+
+        Column(modifier = Modifier.fillMaxSize()) {
+
+            Text(text = "Welcome ${name ?: ""}")
+        }
+
+
+    }
 
 
 }
--- app/src/main/java/com/miempresa/supergamecounter/ui/screen/WelcomeName.kt
+++ app/src/main/java/com/miempresa/supergamecounter/ui/screen/WelcomeName.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.miempresa.supergamecounter.R
@@ -29,6 +28,7 @@ import com.miempresa.supergamecounter.ui.components.MyButton
 fun RootWelcomeName(modifier: Modifier) {
 
     var name by remember { mutableStateOf<String?>(null) }
+
     if (name == null) {
         WelcomeScreenName(
             //Cuando se llame a  ->onNameConfirmed<-
