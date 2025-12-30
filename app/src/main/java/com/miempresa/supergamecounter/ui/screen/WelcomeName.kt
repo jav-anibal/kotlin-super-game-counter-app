1216d9f5eaf2ffdb09df74cdb7c6f1ccdf9b78b2
anibaL <a88anibal@gmail.com>
2025-12-30 06:14:06 +0100

    Creando al UI de WelcomeName

--- app/src/main/java/com/miempresa/supergamecounter/ui/screen/WelcomeName.kt
+++ app/src/main/java/com/miempresa/supergamecounter/ui/screen/WelcomeName.kt
@@ -1,10 +1,22 @@
 package com.miempresa.supergamecounter.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.miempresa.supergamecounter.R
 
 @Composable
 fun RootWelcomeName() {
@@ -35,6 +47,20 @@ fun RootWelcomeName() {
 fun WelcomeScreenName( onNameConfirmed : (String) -> Unit){
 
 
+
+    Column(modifier = Modifier.fillMaxSize().padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        Image(
+            painter = painterResource(id=R.drawable.icon_mascota),
+            contentDescription = "Mascota",
+            modifier = Modifier.size(200.dp)
+        )
+
+
+    }
 
 
 }
